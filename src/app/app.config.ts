ad55e8a666ab528d5e312a150d6344fc7e5a5fde
Arun <arunjothi2599@gmail.com>
2024-06-29 02:44:19 +0530

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebaseConfig)
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"mash-birth-19d92","appId":"1:952764045116:web:399c71634a881de13e7039","storageBucket":"mash-birth-19d92.appspot.com","apiKey":"AIzaSyBBOdlsUF9_MHm8OFJcFQ1voDGI0IION-w","authDomain":"mash-birth-19d92.firebaseapp.com","messagingSenderId":"952764045116","measurementId":"G-R0Z0TEGSVH"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"mash-birth-19d92","appId":"1:952764045116:web:399c71634a881de13e7039","storageBucket":"mash-birth-19d92.appspot.com","apiKey":"AIzaSyBBOdlsUF9_MHm8OFJcFQ1voDGI0IION-w","authDomain":"mash-birth-19d92.firebaseapp.com","messagingSenderId":"952764045116","measurementId":"G-R0Z0TEGSVH"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage()), provideVertexAI(() => getVertexAI()),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBBOdlsUF9_MHm8OFJcFQ1voDGI0IION-w',
+    authDomain: 'mash-birth-19d92.firebaseapp.com',
+    projectId: 'mash-birth-19d92',
+    storageBucket: 'mash-birth-19d92.appspot.com',
+    messagingSenderId: '952764045116',
+    appId: '1:952764045116:web:399c71634a881de13e7039',
+    measurementId: 'G-R0Z0TEGSVH',
+  },
+};
